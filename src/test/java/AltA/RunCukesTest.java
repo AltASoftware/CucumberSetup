85ed66ea70529ab1e9242d809cf8122973b54155
Robert Altena <robert.altena@gmail.com>
2015-11-23 09:33:08 +0900

    Removed cucumber options.

--- src/test/java/AltA/RunCukesTest.java
+++ src/test/java/AltA/RunCukesTest.java
@@ -1,13 +1,8 @@
 package test.java.AltA;
 
-import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-		plugin = {"pretty"},
-		features = "C:\\workspace\\CucumberSetup\\src\\test\\resources\\AltA"
-		)
 public class RunCukesTest {
 }
